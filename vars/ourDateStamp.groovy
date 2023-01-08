4720755fbb6f09e1707b31d36891bfc9ce2dd9e1
Ant Weiss <ant.weiss@gmail.com>
2023-01-08 15:54:25 +0200

    Update ourDateStamp.groovy

--- vars/ourDateStamp.groovy
+++ vars/ourDateStamp.groovy
@@ -2,9 +2,10 @@ def call(format)
 {
    supported_formats = ["ddmmyy", "mmddyy"]
    if (!supported_formats.contains(format)) {
-      println "Unspported format $format"
+      println "Unsupported format $format"
       return 1
    }
+   println format.getClass()
    d = new Date()
-   return Date.format(format)
+   return Date.format(format.toString())
  }  
